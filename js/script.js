63d69fb0bd80d765ec50913d1eebd091c3b48e3a
mnap <learn@10g.pl>
2018-03-15 23:26:05 +0100

    Add phone objects and "class"

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function Phone(brand, price, color) {
+    this.brand = brand;
+    this.price = price;
+    this.color = color;
+}
+Phone.prototype.printInfo = function() {
+    console.log('The phone brand is ' + this.brand +
+        ', color is ' + this.color +
+        ' and the price is ' + this.price + '.');
+};
+
+var samsungGalaxyS6 = new Phone('Samsung', 1199, 'black');
+var iPhone6S = new Phone('Apple', 2250, 'silver');
+var onePlusOne = new Phone('OnePlus', 1399, 'yellow');
+
+samsungGalaxyS6.printInfo();
+iPhone6S.printInfo();
+onePlusOne.printInfo();
